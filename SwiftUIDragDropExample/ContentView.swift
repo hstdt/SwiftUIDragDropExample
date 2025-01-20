87181acc4385393b556646eb13836331fc61ba0e
hstdt <hstdt@users.noreply.github.com>
2025-01-20 11:22:48 +0800

    Initial Commit

---
+++ SwiftUIDragDropExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIDragDropExample
+//
+//  Created by tiandt on 2025/1/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIDragDropExample/SwiftUIDragDropExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDragDropExampleApp.swift
+//  SwiftUIDragDropExample
+//
+//  Created by tiandt on 2025/1/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDragDropExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
